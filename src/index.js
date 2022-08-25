dd686d360d6e6137fe0d1f3a3d252fc6fca6e638
tognia <h.tognia@ziyouma-agency.com>
2022-08-25 18:59:55 +0100

    Authentication Apollo Client

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from './constants';
 
 import {
   ApolloProvider,
@@ -11,14 +13,25 @@ import {
   createHttpLink,
   InMemoryCache
 } from '@apollo/client';
-import { from } from '@apollo/client';
+
 
 const httpLink = createHttpLink({
   uri:'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  // link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
